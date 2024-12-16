cfd80317b16392fa7e243693b2aad4eab81abc2d
Naveed-easv <navnou01@easv365.dk>
2024-12-16 15:35:30 +0100

    Sync main repo changes

---
+++ script.js
@@ -0,0 +1,15 @@
+document.querySelectorAll('.pincode__digit').forEach((input, index, inputs) => {
+    input.addEventListener('input', () => {
+        // Move to the next input if the current input has a value
+        if (input.value.length === 1 && index < inputs.length - 1) {
+            inputs[index + 1].focus();
+        }
+    });
+
+    input.addEventListener('keydown', (e) => {
+        // Handle backspace to move focus to the previous input
+        if (e.key === 'Backspace' && input.value === '' && index > 0) {
+            inputs[index - 1].focus();
+        }
+    });
+});
